896f7362a6f446dee11b6d5fa9ca4e99d3ea9216
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-12-01 01:06:37 +0800

    feat: Determine the length of zx and zy

--- ps4item2.py
+++ ps4item2.py
@@ -5,5 +5,11 @@ def lagrange2D(zx, zy, x, y, w):
     # Determine the length of y
     y_length = len(y)
     
+    # Determine the length of zx
+    zx_length = len(zx)
+    
+    # Determine the lenght of zy
+    zy_length = len(zy)
+    
 
     return 
