2e48d803ba1327bc2d53ccd3c751f87599e52ef3
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-30 15:22:36 +0800

    feat: Generate the Vandermone matrix using the nodes

--- ps4item3.py
+++ ps4item3.py
@@ -7,4 +7,7 @@ def NormalNCWeights(n):
     # Generate the equidistant nodes at the interval [-1, 1]
     x = np.linspace(-1, 1, num = n + 1)
     
+    # Generate the Vandermonde matrix using the nodes
+    V = np.vander(x)
+        
     return normalized_weights
