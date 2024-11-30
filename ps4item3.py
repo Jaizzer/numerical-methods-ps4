dadd2ea6afae6e15cc917babcb617499a7950ad3
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-30 15:08:35 +0800

    feat: Generate the equidistant nodes at the interval [-1, 1]

--- ps4item3.py
+++ ps4item3.py
@@ -1,5 +1,10 @@
+import numpy as np
+
 def NormalNCWeights(n):
     # Initialize the matrix to store the 'n + 1' normalized weights
     normalized_weights = [0 for _ in range(n + 1)]
+    
+    # Generate the equidistant nodes at the interval [-1, 1]
+    x = np.linspace(-1, 1, num = n + 1)
     
     return normalized_weights
