932ff61629c629c1c657c716f9c82449960c9cd6
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-12-01 01:07:52 +0800

    feat: Initialize the matrix to store the evaluated Lagrange characteristic polynomials on the x-axis and y-axis

--- ps4item2.py
+++ ps4item2.py
@@ -11,5 +11,11 @@ def lagrange2D(zx, zy, x, y, w):
     # Determine the lenght of zy
     zy_length = len(zy)
     
+    # Initialize the matrix to store the evaluated Lagrange characteristic polynomials on the x-axis
+    evaluated_characteristic_polynomial_x = [[0 for _ in range(zx_length)] for _ in range(x_length)]
+    
+    # Initialize the matrix to store the evaluated Lagrange characteristic polynomials on the y-axis
+    evaluated_characteristic_polynomial_y = [[0 for _ in range(zy_length)] for _ in range(y_length)]
+    
 
     return 
