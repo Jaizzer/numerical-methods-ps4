2743ae59f49d1025bf38d28a6119c605caf9b015
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-12-01 13:14:33 +0800

    refac: Update variable name to comply to match symbols in the given pset formulas

--- ps4item2.py
+++ ps4item2.py
@@ -1,16 +1,16 @@
 def char_lagrange(z, x):
     # Determine the length of the inputs
-    x_length = len(x)
+    nx = len(x)
     z_length = len(z)
     
     # Initialize the matrix to store the evaluated Lagrange characteristic polynomials
-    evaluated_characteristic_polynomial = [[0 for _ in range(z_length)] for _ in range(x_length)]
+    evaluated_characteristic_polynomial = [[0 for _ in range(z_length)] for _ in range(nx)]
     
     # Evaluate the Lagrange characteristic polynomial at x_k with z_i
-    for k in range(x_length):
+    for k in range(nx):
         for i in range(z_length):
             result = 1
-            for m in range(x_length):
+            for m in range(nx):
                 if m != k:
                     result = result * (z[i] - x[m]) / (x[k] - x[m])
             evaluated_characteristic_polynomial[k][i] = result
@@ -18,16 +18,16 @@ def char_lagrange(z, x):
 
 def lagrange2D(zx, zy, x, y, w):
     # Determine the length of x
-    x_length = len(x)
+    nx = len(x)
     
     # Determine the length of y
-    y_length = len(y)
+    ny = len(y)
     
     # Determine the length of zx
-    zx_length = len(zx)
+    mx = len(zx)
     
     # Determine the lenght of zy
-    zy_length = len(zy)
+    my = len(zy)
     
     # Evaluate the Lagrange characteristic polynomials on the x-axis
     evaluated_characteristic_polynomial_x = char_lagrange(zx, x)
@@ -36,17 +36,17 @@ def lagrange2D(zx, zy, x, y, w):
     evaluated_characteristic_polynomial_y = char_lagrange(zy, y)
 
     # Initialize the vector that will store the evaluated 2D Lagrange interpolation formula
-    evaluated_lagrange_interpolation_formula_2D = [[0 for _ in range(zy_length)] for _ in range(zx_length)]    
+    evaluated_lagrange_interpolation_formula_2D = [[0 for _ in range(my)] for _ in range(mx)]    
 
     # Evaluate the 2D Lagrange interpolation formula
-    for x_index in range(zx_length):
-        for y_index in range(zy_length):
+    for zx_index in range(mx):
+        for zy_index in range(my):
             outer_result = 0
-            for i in range(x_length):
+            for i in range(nx):
                 inner_result = 0
-                for j in range(y_length):
-                    inner_result += w[i][j] * evaluated_characteristic_polynomial_x[i][x_index] * evaluated_characteristic_polynomial_y[j][y_index]
+                for j in range(ny):
+                    inner_result += w[i][j] * evaluated_characteristic_polynomial_x[i][zx_index] * evaluated_characteristic_polynomial_y[j][zy_index]
                 outer_result += inner_result
-            evaluated_lagrange_interpolation_formula_2D[x_index][y_index] = outer_result
+            evaluated_lagrange_interpolation_formula_2D[zx_index][zy_index] = outer_result
             
     return evaluated_lagrange_interpolation_formula_2D   
