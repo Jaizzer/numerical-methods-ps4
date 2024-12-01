2ea6ec45203ae31f23d7440dda11e9535f4ce7a8
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-12-01 10:42:32 +0800

    feat: Use previously created function that evaluates the characteristic Lagrange polynomial

--- ps4item2.py
+++ ps4item2.py
@@ -1,3 +1,21 @@
+def char_lagrange(z, x):
+    # Determine the length of the inputs
+    x_length = len(x)
+    z_length = len(z)
+    
+    # Initialize the matrix to store the evaluated Lagrange characteristic polynomials
+    evaluated_characteristic_polynomial = [[0 for _ in range(z_length)] for _ in range(x_length)]
+    
+    # Evaluate the Lagrange characteristic polynomial at x_k with z_i
+    for k in range(x_length):
+        for i in range(z_length):
+            result = 1
+            for m in range(x_length):
+                if m != k:
+                    result = result * (z[i] - x[m]) / (x[k] - x[m])
+            evaluated_characteristic_polynomial[k][i] = result
+    return evaluated_characteristic_polynomial
+
 def lagrange2D(zx, zy, x, y, w):
     # Determine the length of x
     x_length = len(x)
