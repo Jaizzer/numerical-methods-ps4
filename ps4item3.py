341e6e6da4cd6b39b10523fad137d8b58e7701bb
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-12-01 15:07:51 +0800

    ref: Move quadrature related functions together

--- ps4item3.py
+++ ps4item3.py
@@ -1,36 +1,5 @@
 import numpy as np
 
-def NormalNCWeights(n):    
-    # Initialize an arbitraty interval
-    left_endpoint = -1
-    right_endpoint = 1
-    
-    # Generate the equidistant nodes at the interval [left_endpoint, right_endpoint]
-    x = np.linspace(left_endpoint, right_endpoint, num = n + 1)
-        
-    # Generate the Vandermonde matrix using the nodes
-    V_transposed = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-    for i in range(n + 1):
-        for j in range(n + 1):
-            V_transposed[i][j] = (x[j])**(i)
-    
-    # Initialize the matrix to store the evaluated integral of polynomial p_n(x) on [left_endpoint, right_endpoint]
-    y = [0 for _ in range(n + 1)]
-    
-    # Evaluate the integral of polynomial p_n(x) on [left_endpoint, right_endpoint]
-    for i in range(n + 1):
-        y[i] = ((right_endpoint)**(i + 1) - (left_endpoint)**(i + 1)) /(i + 1) 
-    
-    # Calculate the normalization factor
-    normalization_factor = n / (right_endpoint - left_endpoint)
-    
-    # Solve for the unnormalized weights
-    unnormalized_weights = LU_solve(V_transposed, y)
-    
-    # Normalized the weights
-    normalized_weights = unnormalized_weights * normalization_factor
-
-    return normalized_weights
 
 
 """
@@ -127,6 +96,39 @@ def get_lower_and_upper_triangular_matrices(array):
             U[i][j] = A[i][j]
         
     return [L, U]
+
+
+def NormalNCWeights(n):    
+    # Initialize an arbitraty interval
+    left_endpoint = -1
+    right_endpoint = 1
+    
+    # Generate the equidistant nodes at the interval [left_endpoint, right_endpoint]
+    x = np.linspace(left_endpoint, right_endpoint, num = n + 1)
+        
+    # Generate the Vandermonde matrix using the nodes
+    V_transposed = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+    for i in range(n + 1):
+        for j in range(n + 1):
+            V_transposed[i][j] = (x[j])**(i)
+    
+    # Initialize the matrix to store the evaluated integral of polynomial p_n(x) on [left_endpoint, right_endpoint]
+    y = [0 for _ in range(n + 1)]
+    
+    # Evaluate the integral of polynomial p_n(x) on [left_endpoint, right_endpoint]
+    for i in range(n + 1):
+        y[i] = ((right_endpoint)**(i + 1) - (left_endpoint)**(i + 1)) /(i + 1) 
+    
+    # Calculate the normalization factor
+    normalization_factor = n / (right_endpoint - left_endpoint)
+    
+    # Solve for the unnormalized weights
+    unnormalized_weights = LU_solve(V_transposed, y)
+    
+    # Normalized the weights
+    normalized_weights = unnormalized_weights * normalization_factor
+
+    return normalized_weights
 
 
 def NCQuad(f, a, b, w):
